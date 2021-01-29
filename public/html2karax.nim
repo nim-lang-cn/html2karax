c979d1196230afbf1ac31885f5cd9bb80bf360e0
Antonis Geralis <43617260+planetis-m@users.noreply.github.com>
2021-01-29 23:44:48 +0200

    Refactor html2karax tool

--- public/html2karax.nim
+++ public/html2karax.nim
@@ -1,206 +1,139 @@
-import htmlparser
-import xmltree except escape
-import strtabs  
-import strutils 
-import strformat
-import re
-import unicode except strip
-import sequtils
-import parseopt
+import std / [htmlparser, parseopt, strtabs, strutils, os, xmltree]
 
-var file: string
-var p = parseopt.initOptParser()
+const
+  karaxTemplate = """
+include karax / prelude
 
-while true:
-  parseopt.next(p)
-  case p.kind
-  of cmdEnd: break
-  of cmdLongOption, cmdShortOption:
-    echo p.key
-  of cmdArgument:
-    file = p.key
+proc createDom(): VNode =
+  result = buildHtml:$1
 
-var karaxHtml = ""
+setRenderer createDom
+"""
 
-var tut1 = readFile(file & ".html")
+const
+  nimKeyw = ["addr", "and", "as", "asm",
+    "bind", "block", "break",
+    "case", "cast", "concept", "const", "continue", "converter",
+    "defer", "discard", "distinct", "div", "do",
+    "elif", "else", "end", "enum", "except", "export",
+    "finally", "for", "from", "func",
+    "if", "import", "in", "include", "interface", "is", "isnot", "iterator",
+    "let",
+    "macro", "method", "mixin", "mod",
+    "nil", "not", "notin",
+    "object", "of", "or", "out",
+    "proc", "ptr",
+    "raise", "ref", "return",
+    "shl", "shr", "static",
+    "template", "try", "tuple", "type",
+    "using",
+    "var",
+    "when", "while",
+    "xor",
+    "yield"]
 
-var entity = tut1.findAll(re"&(\w+);")
-tut1 = tut1.multiReplace(entity.zip(entity.mapIt(entityToRune(it.substr(1,it.len-2)).toUTF8)))
+proc toVNode(tag: string): string =
+  case tag
+  of "div":
+    result = "tdiv"
+  of "s":
+    result = "strikethrough"
+  of "var":
+    result = "`var`"
+  of "i":
+    result = "italic"
+  of "b":
+    result = "bold"
+  of "u":
+    result = "underlined"
+  of "object":
+    result = "`object`"
+  else:
+    result = tag
 
-var html = parseHtml tut1
-var body = html.findAll("body")[0][1]
+proc addIndent(result: var string, indent: int, addNewLines: bool) =
+  if addNewLines:
+    result.add("\n")
+  for i in 1 .. indent:
+    result.add(' ')
 
-
-var incremental: int = 2
-template to(t: string) {.dirty.} = 
-  if t == "pre" and body[0].kind == xnText and body[0].text == "\n": body.delete 0
-  var localIndent = incremental
-  karaxHtml.add indent("\n" &  t &  "(", localIndent)
-  var attr: seq[string]
-  if body.attrs != nil:
-    for name,value in body.attrs.pairs:
-      attr.add &"{name}=\"{value}\""
-  var suffix = if body.len != 0 :"):" else: ")"
-  karaxHtml.add attr.join(",") & suffix
-
-  for i in 0..body.len-1:
-    if body[i].kind == xnElement and body[i].len != 0:
-        incremental.inc(2)
-        getXmlNode body[i]
-        incremental = localIndent
-    elif body[i].kind == xnText :
-      var text = body[i].innerText
-      if {'\n','\"','\\'} in text:
-        # if text == "\n": continue
-        text = text.escape
-        if text.find(re"\\x0A") > 1:
-          text = text.replace(re"\\x0A\s+"," ")
-        karaxHtml.add indent("\ntext t " & text , incremental+2)
+proc renderImpl*(result: var string, n: XmlNode, indent = 0, indWidth = 2,
+          addNewLines = true) =
+  if n != nil:
+    case n.kind
+    of xnElement:
+      if indent > 0:
+        result.addIndent(indent, addNewLines)
+      result.add(toVNode(n.tag))
+      result.add('(')
+      if n.attrs != nil:
+        var comma = false
+        for key, val in pairs(n.attrs):
+          if comma: result.add(", ")
+          else: comma = true
+          let isKeyw = key in nimKeyw
+          if isKeyw:
+            result.add('`')
+          result.add(key)
+          if isKeyw:
+            result.add('`')
+          result.add(" = \"")
+          result.add(val)
+          result.add('"')
+      if n.len == 0:
+        result.add(')')
       else:
-        karaxHtml.add indent("\ntext t \"" & text & "\"" , incremental+2)
-      
+        result.add("):")
+        for i in 0 ..< n.len:
+          result.renderImpl(n[i], indent+indWidth, indWidth, addNewLines)
+    of xnText:
+      let text = n.text.strip
+      if text.len > 0:
+        if indent > 0:
+          result.addIndent(indent, addNewLines)
+        result.add("text \"")
+        result.add(text)
+        result.add('"')
+    else: discard
 
-proc getXmlNode(body: var XmlNode) = 
-  case body.tag
-  of "a": to "a"
-  of "abbr": to "abbr"
-  of "acronym": to "acronym"
-  of "address": to "address"
-  of "applet": to "applet"
-  of "area": to "area"
-  of "article": to "article"
-  of "aside": to "aside"
-  of "audio": to "audio"
-  of "b": to "bold"
-  of "base": to "base"
-  of "basefont": to "basefont"
-  of "bdi": to "bdi"
-  of "bdo": to "bdo"
-  of "big": to "big"
-  of "blockquote": to "blockquote"
-  of "body": to "body"
-  of "br": to "br"
-  of "button": to "button"
-  of "canvas": to "canvas"
-  of "caption": to "caption"
-  of "center": to "center"
-  of "cite": to "cite"
-  of "code": to "code"
-  of "col": to "col"
-  of "colgroup": to "colgroup"
-  of "command": to "command"
-  of "datalist": to "datalist"
-  of "dd": to "dd"
-  of "del": to "del"
-  of "details": to "details"
-  of "dfn": to "dfn"
-  of "dialog": to "dialog"
-  of "div": to "tdiv"
-  of "dir": to "dir"
-  of "dl": to "dl"
-  of "dt": to "dt"
-  of "em": to "em"
-  of "embed": to "embed"
-  of "fieldset": to "fieldset"
-  of "figcaption": to "figcaption"
-  of "figure": to "figure"
-  of "font": to "font"
-  of "footer": to "footer"
-  of "form": to "form"
-  of "frame": to "frame"
-  of "frameset": to "frameset"
-  of "h1": to "h1"
-  of "h2": to "h2"
-  of "h3": to "h3"
-  of "h4": to "h4"
-  of "h5": to "h5"
-  of "h6": to "h6"
-  of "head": to "head"
-  of "header": to "header"
-  of "hgroup": to "hgroup"
-  of "html": to "html"
-  of "hr": to "hr"
-  of "i": to "i"
-  of "iframe": to "iframe"
-  of "img": to "img"
-  of "input": to "input"
-  of "ins": to "ins"
-  of "isindex": to "isindex"
-  of "kbd": to "kbd"
-  of "keygen": to "keygen"
-  of "label": to "label"
-  of "legend": to "legend"
-  of "li": to "li"
-  of "link": to "link"
-  of "map": to "map"
-  of "mark": to "mark"
-  of "menu": to "menu"
-  of "meta": to "meta"
-  of "meter": to "meter"
-  of "nav": to "nav"
-  of "nobr": to "nobr"
-  of "noframes": to "noframes"
-  of "noscript": to "noscript"
-  of "object": to "object"
-  of "ol": to "ol"
-  of "optgroup": to "optgroup"
-  of "option": to "option"
-  of "output": to "output"
-  of "p": to "p"
-  of "param": to "param"
-  of "pre": to "pre"
-  of "progress": to "progress"
-  of "q": to "q"
-  of "rp": to "rp"
-  of "rt": to "rt"
-  of "ruby": to "ruby"
-  of "s": to "s"
-  of "samp": to "samp"
-  of "script": to "script"
-  of "section": to "section"
-  of "select": to "select"
-  of "small": to "small"
-  of "source": to "source"
-  of "span": to "span"
-  of "strike": to "strike"
-  of "strong": to "strong"
-  of "style": to "style"
-  of "sub": to "sub"
-  of "summary": to "summary"
-  of "sup": to "sup"
-  of "table": to "table"
-  of "tbody": to "tbody"
-  of "td": to "td"
-  of "textarea": to "textarea"
-  of "tfoot": to "tfoot"
-  of "th": to "th"
-  of "thead": to "thead"
-  of "time": to "time"
-  of "title": to "title"
-  of "tr": to "tr"
-  of "track": to "track"
-  of "tt": to "tt"
-  of "u": to "u"
-  of "ul": to "ul"
-  of "var": to "var"
-  of "video": to "video"
-  of "wbr": to "wbr"
-  else: discard
+const
+  Usage = """html2karax - Convert static html to Karax DSL code.
 
+Usage:
+  html2karax [options] htmlfile
+Options:
+  --out:file    set the output file (default: same name as the input file)
+  --help        show this help
+"""
 
-getXmlNode(body)
+proc writeHelp() =
+  stdout.write(Usage)
+  stdout.flushFile()
+  quit(0)
 
-var karaxTemplate = &"""
-import karax/[karaxdsl,vdom, karax, vstyles]
-import translations
+proc main =
+  var infile, outfile: string
+  for kind, key, val in getopt():
+    case kind
+    of cmdArgument:
+      infile = key.addFileExt(".html")
+    of cmdLongOption, cmdShortOption:
+      case normalize(key)
+      of "help", "h": writeHelp()
+      of "output", "o", "out": outfile = val
+      else: writeHelp()
+    of cmdEnd: assert(false) # cannot happen
 
-proc createDom():VNode = 
-  buildHtml {karaxHtml.strip()}
-    tdiv(style="position:fixed;right:0;top:5%;"):
-      a(onclick = zhCN):span: text "Chinese |"
-      a(onclick = enUS):span: text " English"  
-  
-setRenderer createDom"""
-var outfile = file & "karax.nim"
-writeFile(outfile,karaxTemplate)
+  if infile.len == 0:
+    quit "[Error] no input file."
 
+  if outfile.len == 0:
+    outfile = infile.changeFileExt("nim")
+
+  let parsed = loadHtml(infile)
+  var result = ""
+  renderImpl(result, parsed[0], 4)
+
+  writeFile(outfile, karaxTemplate.format(result))
+
+main()
